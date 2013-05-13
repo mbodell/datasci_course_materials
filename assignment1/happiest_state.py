e3bf1cde96fc30e1dc7bae9835a3c4747d66f1bf
Michael Bodell <mbodell@gmail.com>
2013-05-13 02:50:47 -0700

    Started this from tweet_sentiment.py

---
+++ assignment1/happiest_state.py
@@ -0,0 +1,41 @@
+import sys
+import json
+
+def hw():
+    print 'Hello, world!'
+
+def lines(fp):
+    print str(len(fp.readlines()))
+
+def main():
+    sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[2])
+
+    # create a dictionary of sentiment scores
+    scores = {}
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+    # read in the raw tweets
+    raw_tweet = tweet_file.readlines()
+
+    # capture the tweet scores by state
+    state_scores = {}
+
+    # do the sentiment calculation
+    for i in range(len(raw_tweet)):
+        # get the text of the tweet
+        tweet_txt = json.loads(raw_tweet[i]).get("text", "").encode("UTF-8")
+        # break the tweet into words
+        tweet_txt_words = tweet_txt.split()
+        tweet_score = 0
+        # loop over the words looking up the sentiment of each word
+        for word in tweet_txt_words:
+            tweet_score += scores.get(word, 0)
+        # print the sentiment
+        print float(tweet_score)
+
+
+if __name__ == '__main__':
+    main()
