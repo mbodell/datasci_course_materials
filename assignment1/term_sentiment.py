8f8f7afd49d5b24e1cc13102191e2e54306260a5
Michael Bodell <mbodell@gmail.com>
2013-05-13 01:49:07 -0700

    First pass of term_sentiment copy/paste from tweet_sentiment

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -9,9 +9,33 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    # create a dictionary of sentiment scores
+    scores = {}
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+    # read in the raw tweets
+    raw_tweet = tweet_file.readlines()
+
+    # get the scores for all the tweets, and all the words
+    tweet_scores = []
+    all_words = {}
+    for i in range(len(raw_tweet)):
+        # get the text of the tweet
+        tweet_txt = json.loads(raw_tweet[i]).get("text", "")
+        # break the tweet into words
+        tweet_txt_words = tweet_txt.split()
+        tweet_score = 0
+        # loop over the words looking up the sentiment of each word
+        for word in tweet_txt_words:
+            tweet_score += scores.get(word, 0)
+            all_words[word] = 1
+        # print the sentiment
+        tweet_scores.append(float(tweet_score))
+
+    print tweet_scores
 
 if __name__ == '__main__':
     main()
