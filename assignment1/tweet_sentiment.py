d39e231fd25ce8ed83cf681d5202069eda91d871
Michael Bodell <mbodell@gmail.com>
2013-05-13 00:11:12 -0700

    Got the scores for the sentiment in a dictionary.

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -10,8 +10,26 @@ def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
     hw()
-    lines(sent_file)
-    lines(tweet_file)
+#    lines(sent_file)
+#    lines(tweet_file)
+    scores = {}
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+
+#    tweet_txt = {}
+#    for line in 
+
+    print scores.items()
+
+def foo():
+    afinnfile = open("AFINN-111.txt")
+    scores = {} # initialize an empty dictionary
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    print scores.items() # Print every (term, score) pair in the dictionary
 
 if __name__ == '__main__':
     main()
