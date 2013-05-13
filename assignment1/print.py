233747e6ba9ea97140b35eba660e6ed90d6ca652
Michael Bodell <mbodell@gmail.com>
2013-05-12 19:21:17 -0700

    Solved assignment 0
    page=0 caused problems so had to add 1 to j for the range.

--- assignment1/print.py
+++ assignment1/print.py
@@ -1,11 +1,14 @@
 import urllib
 import json
 
-response = urllib.urlopen("http://search.twitter.com/search.json?q=microsoft")
 
-resp = json.load(response)
+for j in range(9):
+  url = "http://search.twitter.com/search.json?q=microsoft&page=" + str(j + 1)
+  response = urllib.urlopen(url)
+  
+  resp = json.load(response)
 
-results = resp["results"]
+  results = resp["results"]
 
-for i in range(len(results)):
-  print results[i]["text"]
+  for i in range(len(results)):
+    print results[i]["text"]
